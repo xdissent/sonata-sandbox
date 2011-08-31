9de6394da0945631af38bf3db11aa6686513786b
Greg Thornton <gthornton@cjadvertising.com>
2011-08-31 11:07:03 -0500

    Adding pages bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,8 @@ class AppKernel extends Kernel
             new Sonata\BluePrintBundle\SonataBluePrintBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\PageBundle\SonataPageBundle(),
+            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
